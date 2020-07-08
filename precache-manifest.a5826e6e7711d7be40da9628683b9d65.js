7fb11fac63a10ea641e8d8bfefdce392780c529b
Riku Arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2020-07-08 10:27:50 +0900

    Upload sherlock web

---
+++ precache-manifest.a5826e6e7711d7be40da9628683b9d65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7178eda98c6c87e9f9c758461179b55e",
+    "url": "/misc/sherlock/index.html"
+  },
+  {
+    "revision": "1b8e94eb14eac4bf125e",
+    "url": "/misc/sherlock/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "bb7e24e1a53a490117ad",
+    "url": "/misc/sherlock/static/css/main.2b6c205e.chunk.css"
+  },
+  {
+    "revision": "1b8e94eb14eac4bf125e",
+    "url": "/misc/sherlock/static/js/2.8fc04938.chunk.js"
+  },
+  {
+    "revision": "7163cf642829e16a1dd8f5f7df14467c",
+    "url": "/misc/sherlock/static/js/2.8fc04938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7e24e1a53a490117ad",
+    "url": "/misc/sherlock/static/js/main.a366f7e2.chunk.js"
+  },
+  {
+    "revision": "facca382fbecb8e18fd7",
+    "url": "/misc/sherlock/static/js/runtime-main.eb820a2c.js"
+  }
+]);
